9daf15da44e4dd8335c056297eec19897e32f804
djknudson <djknudson@icloud.com>
2020-03-11 11:50:49 -0700

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+    attr_accessor :title, :author, :genre, :page_count, :current_page
+    current_page = 0
+    @current_page = current_page 
+    def initialize(title)
+        @title = title
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
